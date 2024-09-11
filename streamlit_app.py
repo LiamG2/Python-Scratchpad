237079058c84c4468db8932ef164a3541d1bc748
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-11 22:53:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,15 @@ from streamlit_lottie import st_lottie
 
 
 # img_kitajom = Image.open("image_assets/KitaJombanner.png")
-with st.echo():
-    st_lottie("kzrdAgrilI.json", height = 250, key="polyfox")
+# with st.echo():
+    # st_lottie("kzrdAgrilI.json", height = 250, key="polyfox")
+
+
 
 st.title("🎈 My new app")
+
+st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+
 st.write(
     "Let's start building!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
