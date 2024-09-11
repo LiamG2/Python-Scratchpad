54a9f06c20606014a28995f4614800b1894de3cf
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-11 22:57:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from streamlit_lottie import st_lottie
 
 st.title("🎈 My new app")
 
-st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json")
+st_lottie("https://lottie.host/f8f6abbe-7797-48cb-9d72-e3f006de0987/kzrdAgrilI.json")
 
 st.write(
     "Let's start building!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
