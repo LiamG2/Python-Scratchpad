7cb94414fecdb2c82dfc251a63216db0365e4ef5
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-12 09:47:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,12 @@ def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 
-lottie_streamlit = load_lottiefile("/kzrdAgrilI.json")
+# lottie_streamlit = load_lottiefile("/kzrdAgrilI.json")
+lottie_polyfox = "https://lottie.host/f8f6abbe-7797-48cb-9d72-e3f006de0987/kzrdAgrilI.json"
 
 st.title("🎈 My new app")
 
-st_lottie(lottie_streamlit)
+st_lottie(lottie_polyfox, height = 250, key="polyfox")
 
 st.write(
     "Let's start building!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
