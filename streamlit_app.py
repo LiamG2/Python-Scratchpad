6be840c61ff0ec620d8530f408265aecce63d810
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-13 13:09:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,19 +13,6 @@ with st.sidebar: # method for adding widgets to sidebar
 
 
 st.title("🎈 Much longer title of My new app")
-st.radio("", help='Select a number out of 3 choices')
-
-# Tooltips also support markdown
-radio_markdown = '''
-Select a number, you have **3** choices!
-'''.strip()
-
-st.header('Tooltips with Markdown')
-st.radio("Pick a number", [1, 2, 3], help=radio_markdown)
-
-
-
-
 st.write("................................................................................ testing 2 column layoutt")
 
 # prep for [any-num] column layout
@@ -33,7 +20,8 @@ col1, col2 = st.columns(2)
 
 # initiate 1st column
 with col1:
-    st.write("Let's start building!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+    st.write("Let's start building!!! For help and inspiration, head \
+    over to [docs.streamlit.io](https://docs.streamlit.io/).")
     st_lottie(lottie_polyfox_1, height = 100, key="polyfox_2") 
     # note different 'key' name above, needed when using same animation multiple times
     
