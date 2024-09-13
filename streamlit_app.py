fc0ecacc4c156af7e84ec6dea032bfd1f6a428cd
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-13 12:27:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
 
-'''
-def load_lottiefile(filepath: str):
-    with open(filepath, "r") as f:
-        return json.load(f)
-'''
 
 lottie_polyfox_1 = "https://raw.githubusercontent.com/LiamG2/Python-Scratchpad/main/kzrdAgrilI.json"
-# lottie_polyfox_2 = "https://raw.githubusercontent.com/LiamG2/Python-Scratchpad/main/kzrdAgrilI.json"
 
 st.sidebar.header("Sidebar Title")
 
@@ -22,12 +16,11 @@ col1, col2 = st.columns(2)
 # initiate 1st column
 with col1:
     st.write("Let's start building!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-    st_lottie(lottie_polyfox_1, height = 100, key="polyfox_1")
+    st_lottie(lottie_polyfox_1, height = 100, key="polyfox_1") 
+    # note different 'key' name above, needed when using same animation multiple times
     
 # initiate 2nd column
 with col2:
     st.write("Let's start building!!! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
     st_lottie(lottie_polyfox_1, height = 100, key="polyfox_2")
-
-# with col2:
-    # st_lottie(lottie_polyfox, height = 250, key="polyfox")
+    # note different 'key' name above, needed when using same animation multiple times
