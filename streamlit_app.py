9771340b9986a81852c92bb8de977cc396be4576
CADNurd <147918819+LiamG2@users.noreply.github.com>
2024-09-13 13:35:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,22 @@ import streamlit as st
 # noinspection PyUnresolvedReferences
 from streamlit_lottie import st_lottie
 
-lottie_polyfox_1 = ("https://raw.githubusercontent.com/LiamG2/Python"
-                    "-Scratchpad/main/kzrdAgrilI.json") 
+lottie_polyfox_1 = ('https://raw.githubusercontent.com/LiamG2/Python'
+                    '-Scratchpad/main/kzrdAgrilI.json')
 
-st.sidebar.title("Sidebar Title")
-st.sidebar.header("Sidebar Header")
+st.sidebar.title('Sidebar Title')
+st.sidebar.header('Sidebar Header')
 
 with st.sidebar:  # method for adding widgets to sidebar
-    st_lottie(lottie_polyfox_1, height=100, key="polyfox_1")
-    # note different 'key' name above, needed when using same animation 
+    st_lottie(lottie_polyfox_1, height=100, key='polyfox_1')
+    # note different 'key' name above, needed when using same animation
     # \multiple times
 
-st.title("🎈 Much longer title of My new app")
+st.title('🎈 Much longer title of My new app')
 
 st.write(
-    "............................................................."
-    "................... testing 2 column layoutt")
+    '.............................................................'
+    '................... testing 2 column layoutt')
 
 # prep for [any-num] column layout
 col1, col2 = st.columns(2)
@@ -25,20 +25,19 @@ col1, col2 = st.columns(2)
 # initiate 1st column
 with col1:
     st.write(
-        "Let's start building!!! For help and inspiration, head \
-        over to ["
-        "docs.streamlit.io](https://docs.streamlit.io/).")
+        'Let\'s start building!!! For help and inspiration, head \
+        over to ['
+        'docs.streamlit.io](https://docs.streamlit.io/).')
     st_lottie(lottie_polyfox_1, height=100, key="polyfox_2")
-    # note different 'key' name above, needed when using same animation 
+    # note different 'key' name above, needed when using same animation
     # \multiple times
 
 # initiate 2nd column
 with col2:
     st.write(
-        "Let's start building!!! For help and inspiration, head \
-        over to ["
-        "docs.streamlit.io](https://docs.streamlit.io/).")
-    st_lottie(lottie_polyfox_1, height=100, key="polyfox_3")
-    # note different 'key' name above, needed when using same animation 
+        'Let\'s start building!!! For help and inspiration, head \
+        over to ['
+        'docs.streamlit.io](https://docs.streamlit.io/).')
+    st_lottie(lottie_polyfox_1, height=100, key='polyfox_3')
+    # note different 'key' name above, needed when using same animation
     # \multiple times
-    
